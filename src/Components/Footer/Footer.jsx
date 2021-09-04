1960baaa34ac5709b8f8c1455a00c59aa37c4102
batacira <batacira@gmail.com>
2021-09-04 17:37:45 +0200

    header

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,1 +1,11 @@
 import "./Footer.css";
+
+export const Footer = () => {
+  return (
+    <footer class="text-light fixed-bottom">
+        <div class="color" >
+           <div class="text-start p-3 mx-5">© 2021 Copyright BIT</div>
+        </div>
+    </footer>
+  );
+};
