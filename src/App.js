2e1d2d2894c7e9112d1584a709b003c4a62c3fe3
batacira <batacira@gmail.com>
2021-09-04 17:26:28 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import './Components/Header/Header'
+import { Header } from './Components/Header/Header'
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
     </div>
   );
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,1 +1,18 @@
+import React from "react";
+
 import "./Header.css";
+
+export const Header = ({gridView}) => {
+    const f = () => {
+        return gridView = !gridView;
+    }
+
+    return (
+        <div className='header'>
+            <h1>Bit Persons</h1>
+            <button onClick={f}>dada</button>
+            <br />
+            <button><i className="fa-solid fa-user"></i></button>
+        </div>
+    );
+};
