43f6783b83984ae537c2220d6ee07453113f1d37
SlobodanStojkovic <sc.slobodanstojkovic@gmail.com>
2021-09-05 21:06:32 +0200

    Updated Search so it remmembers if we already visited page, and Header with button functions and some style

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,20 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { getUsers } from "../../Services/getUsers";
 
 
 import "./Header.css";
 
-export const Header = ({gridView}) => {
-    const f = () => {
+export const Header = ({ gridView, onRefresh }) => {
+
+    const changeToGridButton = () => {
         return gridView = !gridView;
     }
 
     return (
         <div className='header'>
-            <h1>Bit Persons</h1>
-            <button onClick={f}>dada</button>
-            <br />
-            <button onClick={getUsers}><i className="fa-solid fa-user"></i>Refresh</button>
+            <Link to="/home"><h1>Bit People</h1></Link>
+            <Link to="/about"> <span className="aboutHeader">About</span> </Link>
+            <button className="refreshButton" onClick={onRefresh}><i className="far fa-redo-alt"></i>Refresh</button>
+            <button className="gridButton btn-info" onClick={changeToGridButton}><i className="fas fa-grip-horizontal"></i></button>
         </div>
     );
 };
